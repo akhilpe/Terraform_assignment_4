e6cd1e8385644e76d74db8f362e74d9a297c1ef0
Akhil_Persistent <100367429+akhilpe@users.noreply.github.com>
2022-03-10 12:45:52 +0530

    variables are updated

---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "aws_region" {}
+variable "ami" {}
+variable "instance_type" {}
+variable "subnet_id" {}
+variable "security_groups" {}
+variable "availability_zone" {}
+variable "size" {}
+variable "device_name" {}
